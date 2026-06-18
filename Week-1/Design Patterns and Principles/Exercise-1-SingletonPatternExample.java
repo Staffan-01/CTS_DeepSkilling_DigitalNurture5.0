42c9dcbc5b225fbc35c120c4a2d60e892a49257f
Staffan-01 <infantstaffan2006@gmail.com>
2026-06-18 10:00:00 +0530

    Move design pattern exercises to Design Patterns and Principles folder

---
+++ Week-1/Design Patterns and Principles/Exercise-1-SingletonPatternExample.java
@@ -0,0 +1,29 @@
+class Logger {
+    private static Logger instance;
+
+    private Logger() {
+    }
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
+
+public class SingletonPatternExample {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application started");
+        logger2.log("User logged in");
+
+        System.out.println("Same instance? " + (logger1 == logger2));
+    }
+}
