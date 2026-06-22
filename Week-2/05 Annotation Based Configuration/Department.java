0dda93688f294c97300fd6df90d3351bca671ccd
Staffan-01 <168893069+Staffan-01@users.noreply.github.com>
2026-06-22 10:00:00 +0530

    Remove unnecessary whitespace in Department.java

---
+++ Week-2/05 Annotation Based Configuration/Department.java
@@ -0,0 +1,9 @@
+package com.spring;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Department {
+    public void showDepartment() {
+        System.out.println("Department : Java Full Stack");
+    }
+}
