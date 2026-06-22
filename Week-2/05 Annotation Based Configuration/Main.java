4dc3054163069680b2c93cd9e443577881eeba11
Staffan-01 <168893069+Staffan-01@users.noreply.github.com>
2026-06-22 10:00:00 +0530

    Refactor main method to simplify context initialization

---
+++ Week-2/05 Annotation Based Configuration/Main.java
@@ -0,0 +1,11 @@
+package com.spring;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Employee employee = context.getBean(Employee.class);
+        employee.display();
+    }
+}
